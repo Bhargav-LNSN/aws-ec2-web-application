aae9d363a4502de3c79fb172fe7e211df980e8f7
Bhargav-LNSN <116058139+Bhargav-LNSN@users.noreply.github.com>
2023-06-07 15:42:30 +0530

    Update resources.tf

--- infrastructure/resources.tf
+++ infrastructure/resources.tf
@@ -1,15 +1,8 @@
-terraform {
-  # required_version = "0.12.29"
-}
 
 provider "aws" {
   region     = var.region
   access_key = var.access_key
   secret_key = var.secret_key
-}
-
-provider "random" {
-  version = "=3.0.1"
 }
 
 module "vpc" {
